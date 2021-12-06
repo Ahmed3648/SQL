3f89e616e8aa3f55e96990149892a17fa35d2dc9
Ahmed Buhassan <muharraqee@hotmail.com>
2021-12-06 17:05:15 +0300

    joind more tables

--- Multiple_Tables.sql
+++ Multiple_Tables.sql
@@ -1,34 +1,54 @@
 -- Total Cases vs Total Deaths
---with CTE_deaths as (select location, sum(cast(new_deaths as int)) as new_deaths from dbo.deaths where continent is not null group by location)
---select dbo.cases.location, sum(new_cases) 
---from dbo.cases
---group by dbo.cases.location
---inner join CTE_deaths on dbo.cases.location = CTE_deaths.location;
+-- created two sub-tables: 1) location & new cases, 2) location & new deaths. Then I merged both tables based on location using inner join
 
 create table #grouped_new_cases (
 location varchar (100),
-new_cases int
-)
+new_cases float)
 
 insert into #grouped_new_cases
 select location, sum(new_cases) as NEW
 from dbo.cases
+where continent is not null
 group by location
 
+select *
+from #grouped_new_cases
 
 create table #grouped_new_deaths (
 location varchar (100),
-new_deaths int
-)
+new_deaths float)
 
 insert into #grouped_new_deaths
 select location, sum(cast(new_deaths as int)) as NEW_DEATHS
 from dbo.deaths
+where continent is not null
 group by location
 
-
-select #grouped_new_cases.location, #grouped_new_cases.new_cases, #grouped_new_deaths.new_deaths
+select #grouped_new_cases.location, #grouped_new_cases.new_cases, #grouped_new_deaths.new_deaths,
+	round(#grouped_new_deaths.new_deaths/#grouped_new_cases.new_cases,4)*100  as Death_rate
 from #grouped_new_cases
 inner join #grouped_new_deaths on #grouped_new_cases.location = #grouped_new_deaths.location
-order by #grouped_new_cases.new_cases desc
+where #grouped_new_deaths.new_deaths is not null
+order by Death_rate
+
+
+create table #population (
+location varchar (100),
+population float)
+
+insert into #population
+select location, sum(population) as Total_population
+from dbo.covid
+where continent is not null
+group by location
+
+select #population.population, #grouped_new_cases.new_cases, #grouped_new_deaths.new_deaths,
+	round(#grouped_new_cases.new_cases/#population.population,5) as Cases_percentage,
+	round(#grouped_new_deaths.new_deaths/#population.population,5) as Death_Percentage
+from #population
+inner join #grouped_new_cases on #population.location = #grouped_new_cases.location
+inner join #grouped_new_deaths on #population.location = #grouped_new_deaths.location
+where #grouped_new_deaths.new_deaths is not null
+
+
 
